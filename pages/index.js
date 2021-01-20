f4cb41df5d78e81010acb83564f7d58964681407
davidgeovani <davidgeovani1@gmail.com>
2021-01-20 18:47:17 -0300

    .
    .

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+function HomePage() {
+    return (
+    
+    <div> 
+    <Head>
+        <title> uRestore </title>                 
+    </Head>
+
+    <h1>uRestore</h1>
+    
+    </div>
+    );
+  };
+  
+  export default HomePage
